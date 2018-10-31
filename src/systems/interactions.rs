a1cacb376d09a887cbe06c75a6728e7e3d816d25
Sunjay Varma <varma.sunjay@gmail.com>
2018-10-31 00:24:29 -0400

    Going to the next/prev level if we step on a staircase

--- src/systems/interactions.rs
+++ src/systems/interactions.rs
@@ -2,9 +2,10 @@
 
 use std::borrow::Cow;
 
-use specs::{System, Join, ReadExpect, WriteExpect, ReadStorage, Entities};
+use sdl2::rect::Point;
+use specs::{System, Join, ReadExpect, WriteExpect, ReadStorage, WriteStorage, Entities};
 
-use components::{Position, Movement, MovementDirection};
+use components::{Position, Movement, MovementDirection, CameraFocus};
 use resources::{ActionQueue, Action};
 use map::{GameMap, TileObject, Door};
 
@@ -13,8 +14,9 @@ pub struct InteractionsData<'a> {
     entities: Entities<'a>,
     actions: ReadExpect<'a, ActionQueue>,
     map: WriteExpect<'a, GameMap>,
-    positions: ReadStorage<'a, Position>,
+    positions: WriteStorage<'a, Position>,
     movements: ReadStorage<'a, Movement>,
+    camera_focuses: ReadStorage<'a, CameraFocus>,
 }
 
 #[derive(Default)]
@@ -24,8 +26,7 @@ impl<'a> System<'a> for Interactions {
     type SystemData = InteractionsData<'a>;
 
     fn run(&mut self, data: Self::SystemData) {
-        let InteractionsData {entities, actions, mut map, positions, movements} = data;
-        let level = map.current_level_map_mut();
+        let InteractionsData {entities, actions, mut map, mut positions, movements, camera_focuses} = data;
 
         for (entity, &Position(pos), &Movement {direction, ..}) in (&*entities, &positions, &movements).join() {
             let actions = actions.0.get(&entity).map(Cow::Borrowed).unwrap_or_default();
@@ -35,23 +36,82 @@ impl<'a> System<'a> for Interactions {
                 continue;
             }
 
-            // The tile underneath this position
-            let pos_tile = level.world_to_tile_pos(pos);
+            self.interact_with_tile(&mut map, pos, direction);
+        }
 
-            // The tile adjacent to the current position that we will be interacting with
-            use self::MovementDirection::*;
-            let adjacent = match direction {
-                North => pos_tile.adjacent_north(),
-                South => pos_tile.adjacent_south(level.grid().rows_len()),
-                East => pos_tile.adjacent_east(level.grid().cols_len()),
-                West => pos_tile.adjacent_west(),
-            };
+        // If the camera focus has entered a special tile, we may need to perform an action
 
-            match adjacent.and_then(|adj| level.grid_mut().get_mut(adj).object_mut()) {
-                // Open a door that was previously closed
-                Some(TileObject::Door {state: state@Door::Closed, ..}) => *state = Door::Open,
-                _ => {},
+        // Used to center the camera focus at its destination tile
+        let half_tile_size = map.current_level_map().tile_size() as i32 / 2;
+        let tile_center = Point::new(half_tile_size, half_tile_size);
+        for (Position(ref mut pos), _) in (&mut positions, &camera_focuses).join() {
+            {
+                // Check if we stepped on a ToNextLevel tile
+                let next_level_id = {
+                    let level = map.current_level_map();
+                    let pos = level.world_to_tile_pos(*pos);
+                    level.grid().get(pos).to_next_level_id()
+                };
+                if let Some(id) = next_level_id {
+                    map.to_next_level();
+
+                    let level = map.current_level_map();
+                    // Find the opposite gate with the same ID
+                    let tile_pos = level.grid().find_to_prev_level(id)
+                        .expect("bug: could not find matching previous level gate");
+                    // Find the only traversable adjacent
+                    let target_pos = level.grid().adjacent_positions(tile_pos)
+                        .find(|&pt| level.grid().get(pt).is_traversable())
+                        .expect("bug: no traversable tile beside ToPrevLevel gate");
+                    *pos = target_pos.to_point(level.tile_size() as i32) + tile_center;
+                }
             }
+
+            {
+                // Check if we stepped on a ToPrevLevel tile
+                let prev_level_id = {
+                    let level = map.current_level_map();
+                    let pos = level.world_to_tile_pos(*pos);
+                    level.grid().get(pos).to_prev_level_id()
+                };
+                if let Some(id) = prev_level_id {
+                    map.to_prev_level();
+
+                    let level = map.current_level_map();
+                    // Find the opposite gate with the same ID
+                    let tile_pos = level.grid().find_to_next_level(id)
+                        .expect("bug: could not find matching next level gate");
+                    // Find the only traversable adjacent
+                    let target_pos = level.grid().adjacent_positions(tile_pos)
+                        .find(|&pt| level.grid().get(pt).is_traversable())
+                        .expect("bug: no traversable tile beside ToNextLevel gate");
+                    *pos = target_pos.to_point(level.tile_size() as i32) + tile_center;
+                }
+            }
+        }
+    }
+}
+
+impl Interactions {
+    fn interact_with_tile(&mut self, map: &mut GameMap, pos: Point, direction: MovementDirection) {
+        let level = map.current_level_map_mut();
+
+        // The tile underneath this position
+        let pos_tile = level.world_to_tile_pos(pos);
+
+        // The tile adjacent to the current position that we will be interacting with
+        use self::MovementDirection::*;
+        let adjacent = match direction {
+            North => pos_tile.adjacent_north(),
+            South => pos_tile.adjacent_south(level.grid().rows_len()),
+            East => pos_tile.adjacent_east(level.grid().cols_len()),
+            West => pos_tile.adjacent_west(),
+        };
+
+        match adjacent.and_then(|adj| level.grid_mut().get_mut(adj).object_mut()) {
+            // Open a door that was previously closed
+            Some(TileObject::Door {state: state@Door::Closed, ..}) => *state = Door::Open,
+            _ => {},
         }
     }
 }
