a8dc992858370aac360a81be2d59e94a49d63a8f
Sunjay Varma <varma.sunjay@gmail.com>
2018-11-04 22:33:04 -0500

    Added an AI system for controlling entites automatically

---
+++ src/systems/ai.rs
@@ -0,0 +1,39 @@
+use specs::{System, Join, ReadExpect, ReadStorage, WriteStorage, Entities};
+
+use components::{Movement, BoundingBox, Position, Player, Enemy, Wait};
+use map::GameMap;
+
+#[derive(SystemData)]
+pub struct AIData<'a> {
+    entities: Entities<'a>,
+    map: ReadExpect<'a, GameMap>,
+    movements: WriteStorage<'a, Movement>,
+    bounding_boxes: ReadStorage<'a, BoundingBox>,
+    positions: ReadStorage<'a, Position>,
+    players: ReadStorage<'a, Player>,
+    enemies: ReadStorage<'a, Enemy>,
+    waits: ReadStorage<'a, Wait>,
+}
+
+pub struct AI;
+
+impl<'a> System<'a> for AI {
+    type SystemData = AIData<'a>;
+
+    fn run(&mut self, data: Self::SystemData) {
+        let AIData {
+            entities,
+            map,
+            mut movements,
+            bounding_boxes,
+            positions,
+            players,
+            enemies,
+            waits,
+        } = data;
+        let level = map.current_level_map();
+
+        for (entity, movement, _, ()) in (&entities, &mut movements, &enemies, !&waits).join() {
+        }
+    }
+}
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,8 +2,10 @@ mod animator;
 mod keyboard;
 mod physics;
 mod interactions;
+mod ai;
 
 pub use self::animator::*;
 pub use self::keyboard::*;
 pub use self::physics::*;
 pub use self::interactions::*;
+pub use self::ai::*;
